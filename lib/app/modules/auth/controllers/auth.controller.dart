c1afe2f1387505dab8b8deded47917672bb19d1b
datnguyen <dat.nguyen@enlabsoftware.com>
2023-04-18 17:30:51 +0700

    init project

--- lib/app/modules/auth/controllers/auth.controller.dart
+++ lib/app/modules/auth/controllers/auth.controller.dart
@@ -1,3 +1,4 @@
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:ontari_mobile/app/data/repository/user.repository.dart';
--- lib/app/util/auth_exception.dart
+++ lib/app/util/auth_exception.dart
@@ -14,33 +14,33 @@ enum AuthResultStatus {
 
 class AuthExceptionHandler {
   static dynamic handleException(e) {
-    var status;
+    AuthResultStatus status;
     switch (e.code) {
-      case "invalid-email":
+      case 'invalid-email':
         status = AuthResultStatus.invalidEmail;
         break;
-      case "wrong-password":
+      case 'wrong-password':
         status = AuthResultStatus.wrongPassword;
         break;
-      case "user-not-found":
+      case 'user-not-found':
         status = AuthResultStatus.userNotFound;
         break;
-      case "user-disabled":
+      case 'user-disabled':
         status = AuthResultStatus.userDisabled;
         break;
-      case "ERROR_TOO_MANY_REQUESTS":
+      case 'ERROR_TOO_MANY_REQUESTS':
         status = AuthResultStatus.tooManyRequests;
         break;
-      case "operation-not-allowed":
+      case 'operation-not-allowed':
         status = AuthResultStatus.operationNotAllowed;
         break;
-      case "email-already-in-use":
+      case 'email-already-in-use':
         status = AuthResultStatus.emailAlreadyExists;
         break;
-      case "weak-password":
+      case 'weak-password':
         status = AuthResultStatus.weakPassword;
         break;
-      case "account-exists-with-different-credential":
+      case 'account-exists-with-different-credential':
         status = AuthResultStatus.accountExistsWithDifferentCredential;
         break;
       default:
@@ -56,35 +56,35 @@ class AuthExceptionHandler {
     String errorMessage;
     switch (exceptionCode) {
       case AuthResultStatus.invalidEmail:
-        errorMessage = "Your email address appears to be malformed.";
+        errorMessage = 'Your email address appears to be malformed.';
         break;
       case AuthResultStatus.wrongPassword:
-        errorMessage = "Your password is wrong.";
+        errorMessage = 'Your password is wrong.';
         break;
       case AuthResultStatus.userNotFound:
         errorMessage = "User with this email doesn't exist.";
         break;
       case AuthResultStatus.userDisabled:
-        errorMessage = "User with this email has been disabled.";
+        errorMessage = 'User with this email has been disabled.';
         break;
       case AuthResultStatus.tooManyRequests:
-        errorMessage = "Too many requests. Try again later.";
+        errorMessage = 'Too many requests. Try again later.';
         break;
       case AuthResultStatus.operationNotAllowed:
-        errorMessage = "Signing in with Email and Password is not enabled.";
+        errorMessage = 'Signing in with Email and Password is not enabled.';
         break;
       case AuthResultStatus.emailAlreadyExists:
         errorMessage =
-            "The email has already been registered. Please login or reset your password.";
+            'The email has already been registered. Please login or reset your password.';
         break;
       case AuthResultStatus.weakPassword:
-        errorMessage = "Password too weak";
+        errorMessage = 'Password too weak';
         break;
       case AuthResultStatus.accountExistsWithDifferentCredential:
-        errorMessage = "The account already exists with a different credential";
+        errorMessage = 'The account already exists with a different credential';
         break;
       default:
-        errorMessage = "An undefined Error happened.";
+        errorMessage = 'An undefined Error happened.';
     }
 
     return errorMessage;
